7e7e2fcb0619c3aad9130ae23c117c25b0ed296b
avilapro <mejiasavilaeduardo@gmail.com>
2023-11-10 14:25:37 -0400

    clase 2 - linterna y network plugins nativos

--- clase/src/app/app.module.ts
+++ clase/src/app/app.module.ts
@@ -4,13 +4,15 @@ import { RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
+import { Flashlight } from '@awesome-cordova-plugins/flashlight/ngx';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, Flashlight],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- clase/src/app/tab3/tab3.page.ts
+++ clase/src/app/tab3/tab3.page.ts
@@ -1,12 +1,56 @@
-import { Component } from '@angular/core';
+import { ConnectionStatus } from './../../../node_modules/@capacitor/network/dist/esm/definitions.d';
+import { Component, OnInit } from '@angular/core';
+
+import { Network } from '@capacitor/network';
+
+import { Flashlight } from "@awesome-cordova-plugins/flashlight/ngx";
+
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
-export class Tab3Page {
+export class Tab3Page implements OnInit {
 
-  constructor() {}
+  constructor(public flashlight: Flashlight, private toastController: ToastController) {
+
+  }
+
+  ngOnInit(){
+    Network.addListener('networkStatusChange', (status: ConnectionStatus) => {
+      console.log('Network status changed', status);
+      this.presentToast('middle', status);
+    });
+    this.logCurrentNetworkStatus();
+  }
+
+  async presentToast(position: 'top' | 'middle' | 'bottom', mensaje: ConnectionStatus) {
+        const toast = await this.toastController.create({
+          message: `tipo de conexion: ${mensaje.connectionType}; conectado?: ${mensaje.connected}`,
+          duration: 4000,
+          position: position,
+        });
+
+        await toast.present();
+    }
+
+  logCurrentNetworkStatus = async () => {
+    const status = await Network.getStatus();
+
+    console.log('Network status:', status);
+  };
+
+  switchFlash(evento: any): void{
+    let power : boolean = evento.target.checked;
+    if(power){
+      this.flashlight.switchOn();
+      console.log('linterna encendida');
+    }else{
+      this.flashlight.switchOff();
+      console.log('linterna apagada');
+    }
+  }
 
 }
