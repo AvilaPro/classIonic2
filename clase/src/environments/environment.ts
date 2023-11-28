22698e5c87d2dbf1d41097d547886f9173eee262
avilapro <mejiasavilaeduardo@gmail.com>
2023-11-28 12:25:44 -0400

    clase 4 - geolocation y google maps - p1

--- clase/src/environments/environment.ts
+++ clase/src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  tknGoo : 'AIzaSyAH0I32k9QFCUdbArYYmZSkAkMaHX4WvA0'
 };
 
 /*
